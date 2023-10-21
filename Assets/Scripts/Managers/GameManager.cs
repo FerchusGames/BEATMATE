562b1efff0c43197b25320c8fdb78bb10b0e50aa
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2023-10-21 13:09:38 -0600

    Highlight

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public GameState GameState;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        ChangeState(GameState.GenerateGrid);
+    }
+
+    public void ChangeState(GameState newState)
+    {
+        GameState = newState;
+        switch (newState)
+        {
+            case GameState.GenerateGrid:
+                GridManager.Instance.GenerateGrid();
+                break;
+            case GameState.HeroesTurn:
+                break;
+            case GameState.EnemiesTurn:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+    }
+}
+
+public enum GameState
+{
+    GenerateGrid = 0,
+    HeroesTurn = 1,
+    EnemiesTurn = 2,
+    Dialogue = 3,
+}
