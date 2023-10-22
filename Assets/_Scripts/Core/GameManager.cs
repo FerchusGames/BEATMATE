f970a6a66e992ac4099951da286be70396909c85
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2023-10-22 13:05:00 -0600

    Removed Unnecessary Scripts

--- Assets/_Scripts/Core/GameManager.cs
+++ Assets/_Scripts/Core/GameManager.cs
@@ -1,18 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance;
+
+    public GameState State;
+
+    public static event Action<GameState> OnGameStateChange;
+
+    void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateGameState(GameState newState)
     {
-        
+        State = newState;
+
+        switch (newState)
+        {
+            case GameState.Dialogue:
+                HandleDialogue();
+                break;
+            case GameState.PlayerTurn:
+                HandlePlayerTurn();
+                break;
+            case GameState.EnemyTurn:
+                HandleEnemyTurn();
+                break;
+            case GameState.Victory:
+                HandleVictory();
+                break;
+            case GameState.Defeat:
+                HandleDefeat();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+
+        OnGameStateChange?.Invoke(newState);
     }
+
+    private void HandleDefeat()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleVictory()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleEnemyTurn()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandlePlayerTurn()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleDialogue()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public enum GameState
+{
+    Dialogue = 0,
+    PlayerTurn = 1,
+    EnemyTurn = 2,
+    Victory = 3,
+    Defeat = 4,
 }
--- Assets/_Scripts/Movement/Mover.cs
+++ Assets/_Scripts/Movement/Mover.cs
@@ -2,5 +2,8 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    
+    public void Move(Vector2 movement)
+    {
+        transform.position += (Vector3)movement;
+    }
 }
